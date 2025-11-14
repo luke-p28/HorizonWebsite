951b801c8a91b110d1d200db197f64a989279929
Luke Powell <1powellluk@hdsb.ca>
2025-11-13 20:07:14 -0500

    Removed hard-coded paths
    Made it so that run.bat works regardless of its absolute path, as long as the active directory is the project's root directory.

--- run.bat
+++ run.bat
@@ -1,5 +1,8 @@
+FOR /F "usebackq tokens=* delims=" %%A IN (`powershell -command Get-Location`) DO (
+    SET "data=%%A"
+)
 if "%1"== "alt" (
-    powershell -command powershell -command {start-job {cd c:/users/luke0/projects/javascript/site; httpserver}; cd c:/users/luke0/projects/javascript; node server.js alt} 
+    powershell -command powershell -command {start-job -scriptblock {echo $input; cd $input; httpserver} -inputobject %data%/site; receive-job 1; node server.js alt}
 ) else (
-    powershell -command powershell -command {start-job {cd c:/users/luke0/projects/javascript/site; httpserver}; cd c:/users/luke0/projects/javascript; node server.js} 
+    powershell -command powershell -command {start-job -scriptblock {echo $input; cd $input; httpserver} -inputobject %data%/site; receive-job 1; node server.js}
 )
